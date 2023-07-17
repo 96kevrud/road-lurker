fd27f821d11381fc95008c0d25eb20be4bcf749c
96kevrud <kevinleerudnick@gmail.com>
2023-07-17 14:26:37 +0200

    Remove private api key

--- python-api-test/api-test.py
+++ python-api-test/api-test.py
@@ -1,14 +1,20 @@
 import requests
 
+api_key = "YOUR_API_KEY"
+
 url = "https://api.trafikinfo.trafikverket.se/v2/data.json"
 headers = {'Content-Type':'text/xml'}
 
-with open("post.xml") as xml:
+with open("post.xml") as xml_file:
+    xml_string = xml_file.read()
+    xml_string = xml_string.replace("APIKEY", api_key)
+
     response = requests.post(
         url=url,
-        data=xml,
+        data=xml_string,
         headers=headers
     )
+
     print(response.status_code)
     result = response.json()["RESPONSE"]["RESULT"][0]
     cameras = result["Camera"]
