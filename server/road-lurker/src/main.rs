75f986050c96223ab7e73b3b2ed39652ce94788a
TheAio <kylerudnick04@duck.com>
2023-08-10 20:28:35 +0200

    Rocket can now render a 404 error incorrectly

--- server/road-lurker/src/main.rs
+++ server/road-lurker/src/main.rs
@@ -1,11 +1,26 @@
 #[macro_use] extern crate rocket;
 
+
+use rocket::{fs::NamedFile, response::Redirect};
+use std::path::{Path, PathBuf};
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Redirect {
+    let redirect = Redirect::to(uri!("/index.html"));
+    redirect
+}
+
+#[get("/index.html")]
+async fn home () -> Option<NamedFile> {
+    NamedFile::open("client/index.html").await.ok()
+}
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("client").join(file)).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, home, files])
 }
